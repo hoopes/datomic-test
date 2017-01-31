cb4465e0a719c0edee934d615c49a43487b67f33
Matthew Hoopes <hoopes@rhiza.com>
2017-01-30 20:18:39 -0500

    more docs

--- src/datomic_test/components/datomic.clj
+++ src/datomic_test/components/datomic.clj
@@ -7,8 +7,20 @@
 (defstate conn :start (c/connect!))
 
 (defn transact [tx-data]
+  "Create a transaction in datomic. This is the 'PUT' of the system
+
+  FIXME: What am i returning?"
   (client/transact conn {:tx-data tx-data}))
 
 (defn query [query]
+  "Use core.async to take the value off of the channel returned
+    by client/q. This function should return the actual value(s)
+    queried for.
+
+  Requires a datalog query (a list of them?) as its argument.
+
+  This is basically the 'GET' of the system.
+
+  FIXME: More documentation"
   (let [db (client/db conn)]
     (<!! (client/q conn {:query query :args [db]}))))
